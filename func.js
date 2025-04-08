445147004753e4ab17ae942b058be54fc50f478f
Amogh1610 <amogh.choudhary1016@gmail.com>
2025-04-09 05:03:36 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    // Display a heartfelt message when the button is clicked
+    document.getElementById('messageSection').style.display = 'block';
+    document.getElementById('showMessage').style.display = 'none';
+    document.getElementById('thankYou').style.display = 'inline-block';
+}
+
+function thankYouMessage() {
+    alert("This not the final gift. Meet Amogh at 10:30. I love you.");
+}
